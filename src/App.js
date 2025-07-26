f758d15d1fe8ddc5b69bf1c4b2b395cdcfeab8d7
SaaDeDD1neHab1b <saadeddinehabib.business@gmail.com>
2025-07-26 18:45:29 +0200

    Navbar v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function Navbar(props) {
     return (
-        <nav className="">
+        <nav className="bg-blue-800 p-10 w-full">
             <Button url="#miao" title="Home" />
             <Button url="#miao" title="About" />
             <Button url="#miao" title="Projects" />
