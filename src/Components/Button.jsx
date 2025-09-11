11756e4dfdcd1c08bb3a8714ada3ff80b383bd90
SaaDeDD1neHab1b <saadeddinehabib.business@gmail.com>
2025-09-11 18:15:06 +0200

    Implemented basic navigation.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,8 +2,7 @@ import "./CSS/styles.css"
 
 
 export default function Button(props) {
-    const path = window.location.pathname
     return (
-        <a className={path === props.url ? "active-btn": "link-btn" } href={props.url}>{props.title}</a>
+        <a className="link-btn"  href={props.url}>{props.title}</a>
     );
 }
