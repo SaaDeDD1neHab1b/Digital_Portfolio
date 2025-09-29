26db05521a4bcbac04439ec3ae06c5f1d93430f4
SaaDeDD1neHab1b <saadeddinehabib.business@gmail.com>
2025-09-29 21:08:22 +0200

    Implemented Footer component  successfully

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,14 +5,16 @@ import IconLink from "./IconLink";
 export default function Footer(props) {
     return (
         <footer>
-            <IconLink url="https://www.linkedin.com" color="blue" 
+            <IconLink url="https://www.linkedin.com" color="#193cb8" 
+            name="BsLinkedin" size="2em"/>
+            <IconLink url="https://www.instagram.com" color="purple" 
             name="FaInstagram" size="2em"/>
-            <IconLink url="https://www.linkedin.com" color="purple" 
-            name="FaInstagram" size="2em"/>
-            <IconLink url="https://www.linkedin.com" color="white" 
-            name="FaInstagram" size="2em"/>
-            <IconLink url="https://www.linkedin.com" color="red" 
-            name="FaInstagram" size="2em"/>
+            <IconLink url="https://www.github.com" color="black" 
+            name="FaGithub" size="2em"/>
+            <IconLink url="https://mail.google.com" color="red" 
+            name="IoIosMail" size="2em"/>
+            <h1>© 2025 - 2025 www.saadeddineHabib.com - All Rights Reserved.</h1>
+            <br />
         </footer>
     );
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,8 +3,19 @@ import "../Components/CSS/styles.css"
 
 export default function About(props) {
     return (
+        <div>
+            <div className="side" />
         <main>
-            <h1>ABOUT</h1>
+            <h1>Your great and best option to build websites!</h1>
+            <h2>Saadeddine Habib | software devoloper</h2>
+
+            <h3>Biografy</h3>
+            <p>
+                
+            </p>
         </main>
+           <div className="side" />
+        </div>
+
     );
 }
