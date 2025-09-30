ba3a98f2a15322ed4bc489ca7e825da2414c04e7
SaaDeDD1neHab1b <saadeddinehabib.business@gmail.com>
2025-09-30 21:57:33 +0200

    Implementando el timeline goals

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,12 @@
 import "../Components/CSS/styles.css"
 
-
 export default function About(props) {
     return (
         <main>
             <h1>Your great and best option to build websites! </h1>
             <h2>Saadeddine Habib | software devoloper</h2>
-
+            <br />
+            
             <h3>Biografy</h3>
             <p>
                 Born on October 19, 2005, Saadeddine Habib has forged a remarkable path in the field of technology. He began his academic training with a Vocational Training Degree in Microcomputer Systems and Networks, where he gained a solid foundation in the functioning of computer systems and communication networks. Subsequently, he continued his education with a Higher Vocational Training Degree in Multiplatform Application Development (DAM), specializing in the creation of versatile and efficient software.
@@ -17,11 +17,12 @@ export default function About(props) {
                 <br />
                 Despite his impressive background, Saadeddine Habib maintains a sense of humor that sets him apart. After all, what developer hasn’t spent hours trying to find a bug in the code only to realize they forgot to close a parenthesis? Programming is an art, but sometimes it feels more like a game of hide and seek!
             </p>
-
-            <div>
-                I'm implementing the img block and after the multiselect achievment block
-                <div></div>
-                <img src="logo.svg" alt="" />
+            <br />
+            <br />
+            <div className="realtiveEx">
+                {/*Hay que implementar el timeline goals */ }
+                <div className="divAbs"></div>
+                <img className="imgAbs" src={require("../img/montaña.jpg")} alt="no se muestra" />
             </div>
         </main>
     );
