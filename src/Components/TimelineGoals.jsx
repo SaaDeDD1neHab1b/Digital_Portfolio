d85288a1d0b5f4062dbce88ffe24253e14cca133
SaaDeDD1neHab1b <saadeddinehabib.business@gmail.com>
2025-10-09 17:38:50 +0200

    Fixing img size scalling

--- src/Components/TimelineGoals.jsx
+++ src/Components/TimelineGoals.jsx
@@ -10,61 +10,72 @@ export default function TimelineGoals(props) {
             "ID": 0,
             "Title-btn": "Proyecto A",
             "Heading": "Desarrollo de Aplicación Móvil",
-            "Description": "Completé el desarrollo de una aplicación móvil que permite a los usuarios gestionar sus tareas diarias de manera eficiente."
+            "Description": "Completé el desarrollo de una aplicación móvil que permite a los usuarios gestionar sus tareas diarias de manera eficiente.",
+            "Img": require("../img/p-1.jpg")
         },
         {
             "ID": 1,
             "Title-btn": "Proyecto B",
             "Heading": "Implementación de API",
-            "Description": "Diseñé e implementé una API RESTful que facilita la interacción entre el frontend y el backend de la aplicación."
+            "Description": "Diseñé e implementé una API RESTful que facilita la interacción entre el frontend y el backend de la aplicación.",
+            "Img": require("../img/p-2.jpg")
+
         },
         {
             "ID": 2,
             "Title-btn": "Proyecto C",
             "Heading": "Mejora de Rendimiento",
-            "Description": "Optimicé el rendimiento de la aplicación, reduciendo el tiempo de carga en un 40% mediante la implementación de técnicas de caching."
+            "Description": "Optimicé el rendimiento de la aplicación, reduciendo el tiempo de carga en un 40% mediante la implementación de técnicas de caching.",
+            "Img": require("../img/p-3.jpg")
         },
         {
             "ID": 3,
             "Title-btn": "Proyecto D",
             "Heading": "Migración a la Nube",
-            "Description": "Lideré la migración de la infraestructura del proyecto a la nube, mejorando la escalabilidad y la disponibilidad del servicio."
+            "Description": "Lideré la migración de la infraestructura del proyecto a la nube, mejorando la escalabilidad y la disponibilidad del servicio.",
+            "Img": require("../img/p-4.jpg")
         },
         {
             "ID": 4,
             "Title-btn": "Proyecto E",
             "Heading": "Integración de CI/CD",
-            "Description": "Implementé un pipeline de integración y entrega continua que automatiza las pruebas y despliegues, mejorando la eficiencia del equipo."
+            "Description": "Implementé un pipeline de integración y entrega continua que automatiza las pruebas y despliegues, mejorando la eficiencia del equipo.",
+            "Img": require("../img/p-5.jpg")
         },
         {
             "ID": 5,
             "Title-btn": "Proyecto F",
             "Heading": "Desarrollo de Funcionalidad de Chat",
-            "Description": "Implementé una funcionalidad de chat en tiempo real utilizando WebSockets, mejorando la comunicación entre los usuarios."
+            "Description": "Implementé una funcionalidad de chat en tiempo real utilizando WebSockets, mejorando la comunicación entre los usuarios.",
+            "Img": require("../img/p-6.jpg")
         },
         {
             "ID": 6,
             "Title-btn": "Proyecto G",
             "Heading": "Rediseño de Interfaz de Usuario",
-            "Description": "Realicé un rediseño completo de la interfaz de usuario, mejorando la experiencia del usuario y la accesibilidad."
+            "Description": "Realicé un rediseño completo de la interfaz de usuario, mejorando la experiencia del usuario y la accesibilidad.",
+            "Img": require("../img/p-7.jpg")
         },
         {
             "ID": 7,
             "Title-btn": "Proyecto H",
             "Heading": "Automatización de Pruebas",
-            "Description": "Desarrollé un conjunto de pruebas automatizadas que cubren el 90% del código, asegurando la calidad del software."
+            "Description": "Desarrollé un conjunto de pruebas automatizadas que cubren el 90% del código, asegurando la calidad del software.",
+            "Img": require("../img/p-8.jpg")
         },
         {
             "ID": 8,
             "Title-btn": "Proyecto I",
             "Heading": "Documentación del Proyecto",
-            "Description": "Creé una documentación exhaustiva del proyecto, facilitando la comprensión y el mantenimiento por parte de futuros desarrolladores."
+            "Description": "Creé una documentación exhaustiva del proyecto, facilitando la comprensión y el mantenimiento por parte de futuros desarrolladores.",
+            "Img": require("../img/p-9.jpg")
         },
         {
             "ID": 9,
             "Title-btn": "Proyecto J",
             "Heading": "Implementación de Seguridad",
-            "Description": "Integré medidas de seguridad en la aplicación, como la autenticación de dos factores y la encriptación de datos sensibles."
+            "Description": "Integré medidas de seguridad en la aplicación, como la autenticación de dos factores y la encriptación de datos sensibles.",
+            "Img": require("../img/p-10.jpg")
         }
     ]
 
@@ -94,7 +105,8 @@ export default function TimelineGoals(props) {
 
 
             </div>
-            <img className="imgAbs" src={require("../img/montaña.jpg")} alt="no se muestra" />
+            {/* Arreglar el problema de tamaño de las imagenes de mas altura que anchura*/}
+            <img className="imgAbs" src={productosGaming[selectedOption].Img} alt="no se muestra" />
         </div>
     );
 }
